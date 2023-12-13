5db534c5ed28df9a64bfcabfc1e122d292dc5fb4
monsieurlamah <monsieurlamah2024@gmail.com>
2023-12-13 11:45:45 +0000

    solucopartie2

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -56,12 +56,18 @@ $('#commentForm').submit(function(e){
 })
 
 
-// Filter no-refresh
+// Filter by category and vendor no-refresh 
 $(document).ready(function(){
-    $(".filter-checkbox").on("click", function(){
+    $(".filter-checkbox, #price-filter-btn").on("click", function(){
         console.log("Le checkbox a été touché");
 
         let filter_object = {}
+
+        let min_price = $("#max_price").attr("min")
+        let max_price = $("#max_price").val()
+
+        filter_object.min_price = min_price;
+        filter_object.max_price = max_price;
 
         $(".filter-checkbox").each(function() {
             let filter_value = $(this).val()
@@ -91,5 +97,33 @@ $(document).ready(function(){
 
         })
 
+    })
+
+    //Filter by price
+    $("#max_price").on('blur', function() {
+        let min_price = $(this).attr("min")
+        let max_price = $(this).attr("max")
+        let current_price = $(this).val()
+
+        // console.log("Le prix actuel est ", current_price);
+        // console.log("Le prix min est ", min_price);
+        // console.log("Le prix max est ", max_price);
+
+        if (current_price < parseInt(min_price) || current_price > parseInt(max_price)) {
+            // console.log('Error price occured');
+
+            min_price = Math.round(min_price * 100)/100
+            max_price = Math.round(max_price * 100)/100
+
+            // console.log("Le prix min est--- ", min_price);
+            // console.log("Le prix max est--- ", max_price);
+            alert("Le prix doit être compris entre " + min_price + " GNF " + " et " + max_price + ' GNF')
+            $(this).val(min_price)
+            $('#range').val(min)
+
+
+            $(this).focus()
+            return false
+        }
     })
 })
